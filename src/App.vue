fa4a0b5e064b6635674f11478fb0cc9d951ac603
GugsTI <gustavobuenodaccb@gmail.com>
2024-03-22 10:58:17 -0300

    atualizado001

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { ref } from 'vue';
+const nome = ref('');
+const idade = ref('');
+
+
+
+
+
+
+const dados = function(){
+  alert( `Seu Nome: ${nome.value}`)
+   alert( `Sua idade: ${idade.value}`)
+}
+
+
+
+</script>
+<template>
+  <p>Nome:</p><input class="inp" v-model="nome" />
+  <p>idade:</p><input class="inp" type="number" v-model="idade">
+
+  <button @click="dados" >Resultado</button>
+  
+
+  
+</template>
+
+<style>
+.inp {
+  border-radius: 5px;
+  color: white;
+  background-color: blue;
+}
+</style>
